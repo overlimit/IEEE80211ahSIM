dc0869bdfe33100c848b2e67ef2f3bff2a43d476
overlimit <gn01305293@gmail.com>
2017-03-14 17:41:12 +0800

    next: event controller

--- IEEE80211ahSIM.py
+++ IEEE80211ahSIM.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[85]:
+# In[10]:
 
 class Node:
     'Common base class for all nodes'
@@ -9,62 +9,72 @@ class Node:
     Time unit: slot time = 52
     data rate: 1 Mbps
     '''
-    sendRequest = 531 
+    sendPacket = 531 
     waitDIFS = 564 
     waitResponse = 315 #wait for ACK
     countDownBackoff = 615
     channelBusy = 213
     waitOthersACK = 843
     READY = 534
-    waitNextState = 123 #part 1 end
+    HALT = 5843
     
     nodeCounts = 0
+    packetCount = 0
     DEVICE = "node"
     SIFS = 3
     DIFS = 5
     ACKTime = 5
     from random import randint
     
-    def __init__(self, x, y, packetLength, samplingRate):
+    def __init__(self, x, y, packetLength, samplingRate, loop):
         self.x = x
         self.y = y
         self.packetLength = packetLength
         self.samplingRate = samplingRate
         self.nodeInRange = []
         self.group = -1
+        self.AID = -1
         self.state = Node.waitDIFS
         self.timeToNextTask = Eventlist.currentTime + DIFS
         self.nav = 0
-        self.backoffStage = 3
-        self.backoffTime = 0
+        self.checkACK = 0
+        self.backoffStage = 4
+        self.backoffTime = randint(0, 2 ** max(self.backoffStage, 10))
         self.tranTime = fix((packetLength*8)/52)
+        self.collisionTimes = 0
+        self.loop = loop
         Node.nodeCounts += 1
         
-    def sendRequest(self):
+    def sendPacket(self):
         '''
         to AP: send request
         to other nodes: message "transmitting"
         '''
+        Node.packetCount += 1
         self.timeToNextTask = Eventlist.currentTime + self.tranTime + SIFS
-        self.state = Node.sendRequest
+        self.state = Node.sendPacket
         for x in range(len(self.nodeInRange)):
             self.nodeInRange[x].receivePacket(self.tranTime)
         return self
             
     
     def receivePacket(self, time):
-        'receivePacket'
-        if self.state == Node.READY:
-            continue
-        self.state = Node.waitTransmit
-        self.timeToNextTask = Eventlist.currentTime + time
+        'Carrier sense'
+        if self.state == Node.READY: #While sending packet simultaneously
+            print "sending packet simultaneously\n"
+            return
+        if self.state == Node.receiveACK:  #collision
+            self.checkACK = 0
+        self.state = Node.channelBusy
+        if self.timeToNextTask < (Eventlist.currentTime+ time):
+            self.timeToNextTask = Eventlist.currentTime + time
         
     def changeState(self):
-        'change state'
+        "change state when it's the node's turn"
         return {
-            sendRequest: self.toWaitResponse(),
+            sendPacket: self.toWaitResponse(),
             waitResponse: self.toCheckACK(),
-            waitDIFS: self.readyToWork(),
+            waitDIFS: self.toBackoff(),
             countDownBackoff: self.readyToWork(),
             channelBusy: self.setNav(),
             waitOthersACK: self.toBackoff()            
@@ -77,20 +87,33 @@ class Node:
     def toCheckACK(self):
         'check whether ACK is transmitted sucessfully'
         'if failed => toBackoff'
-        'if success => part 1 end'
+        if self.checkACK == 1:
+            'transmitted sucessfully'
+            if self.loop:
+                self.state = Node.waitDIFS
+                self.timeToNextTask = Eventlist.currentTime + DIFS
+            else:
+                self.state = Node.HALT
+                self.timeToNextTask = Eventlist.maxTime
+        elif self.checkACK == 0:
+            Eventlist.collision += 1
+            self.collisionTimes += 1
+            self.toBackoff()
         
     def readyToWork(self):
+        self.backoffStage = 3
+        self.backoffTime = 0
         self.state = Node.READY
         
     def setNav(self):
-        self.nav = SIFS + ACKTime
-        self.state = waitOthersACK
+        self.nav = SIFS + ACKTime + DIFS
+        self.state = Node.waitOthersACK
         self.timeToNextTask = Eventlist.currentTime + self.nav
         
     def toBackOff(self):
         self.nav = 0
-        self.state = countDownBackoff
-        if self.backoffTime = 0:
+        self.state = Node.countDownBackoff
+        if self.backoffTime == 0:
             self.backoffStage += 1
             self.backoffTime = randint(0, 2 ** max(self.backoffStage, 10))
             self.timeToNextTask = Eventlist.currentTime + self.backoffTime
@@ -114,7 +137,7 @@ class Node:
         
 
 
-# In[6]:
+# In[13]:
 
 class AP:
     'common access point'
@@ -123,24 +146,69 @@ class AP:
     DIFS = 5
     ACKTime = 5
     
+    IDLE = 655
+    sendPacket = 531 
+    channelBusy = 213
+    READY = 534
+    
     def __init__(self, x, y, numOfGroup):
         self.x = x
         self.y = y
         self.group = []
-        self.state = "grouping"
-        self.timeToNextTask = 100000
-        for x in range(numOfGroup):
+        self.state = AP.IDLE
+        self.timeToNextTask = Eventlist.maxTime
+        self.nodeInRange = []
+        for groups in range(numOfGroup):
             self.group.append([0])
             
-    def receivePacket(self):
+    def receivePacket(self, node):
         'receivePacket'
+        if self.state == AP.IDLE:
+            self.state = AP.channelBusy
+            self.timeToNextTask = Eventlist.currentTime + node.tranTime + SIFS
+            self.respondTarget = node
+        elif self.state == AP.channelBusy:
+            'collision'
+            self.state = AP.IDLE
+            self.respondTarget = 0
+            self.timeToNextTask = Eventlist.maxTime
+            
+    def sendACK(self):
+        'send ACK'
+        self.timeToNextTask = Eventlist.current + ACKTime
+        self.state = AP.sendPacket
+        for x in range(len(self.nodeInRange)):
+            self.nodeInRange[x].receivePacket(ACKTime)
+        self.respondTarget.checkACK = 1
+        
+    def changeState(self):
+        return {
+            IDLE: self.processEnd(),
+            sendPacket: self.toIDLE(),
+            channelBusy: self.readyToWork(),
+        }[self.state]
+    
+    def processEnd(self):
+        print "process end"
+        return 0
+    
+    def toIDLE(self):
+        self.respondTarget = 0
+        self.state = IDLE
+        self.timeToNextTask = Eventlist.maxTime
+        
+    def readyToWork(self):
+        self.state = READY
+        
+    def calcRange(self, node):
+        self.nodeInRange.append(node)
 
     def displayStatus(self):
         print "x: %d, y: %d,\nType: %s, number of group: %d" %(self.x, self.y, self.device, len(self.group))
         
 
 
-# In[13]:
+# In[17]:
 
 from random import choice, randint
 from numpy import *
@@ -154,20 +222,26 @@ samplingRateList = [2, 4, 8, 16]
 points = []
 Node.nodeCounts = 0
 
+eventController = Eventlist(1)
+
 points.append(AP(0,0,4))
 
 while Node.nodeCounts < 10:
     x, y = randint(-RADIUS,RADIUS), randint(-RADIUS,RADIUS)
     if (x*x + y*y) <= SQUERE_RADIUS:
-        points.append(Node(x, y, 32, choice(samplingRateList)))        
+        points.append(Node(x, y, 32, choice(samplingRateList),1))        
 
 for node1 in range(1, len(points)):
+    points[0].calcRange(points[node1])
     for node2 in range(node1+1, len(points)):
         points[node1].calcRange(points[node2])        
     #points[node1].displayNodesInRange()
+    
+while(Eventlist.currentTime <= Eventlist.maxTime):
+    
 
 
-# In[14]:
+# In[23]:
 
 #print points[5].timeToNextTask
 print points[:]
@@ -176,12 +250,16 @@ print points[:]
 # In[15]:
 
 class Eventlist:
+    from numpy import *
     currentTime = 0
-    def __init__(self):
+    collision = 0
+    maxTime = 0
+    def __init__(self, endTime):
         self.event = []
+        Eventlist.maxTime = fix(endTime*1000000/52)
         
-    def insert(e):
-        Eventlist.event.append(e)
+    def insert(self, e):
+        self.event.append(e)
     
     def showList():
         for x in range(len(event)):
@@ -189,7 +267,7 @@ class Eventlist:
     
 
 
-# In[84]:
+# In[1]:
 
 class Test:
     var = 'a'
